28d0eba995febb17c55adf505bb2adb27be85f30
= <bot.trial001@gmail.com>
2024-10-03 10:28:47 +0500

    ädded function in new.js

--- new.js
+++ new.js
@@ -1,13 +1,20 @@
 
 
 // Function to clear all tasks
-function clear_all_tasks() {
+function clear_all_tasks(num) {
     console.log("clear all tasks function");
+    if(num>0)
+    {
+        console.log("positive");
+    }
+    else{
+        console.log("negative");
+    }
 
 }
 
 // Function to save tasks to localStorage
 
-clear_all_tasks();
+clear_all_tasks(3);
 // Display tasks on page load
 
