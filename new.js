522fab9ae57217f52f74e47b35667aa34dc22b06
amoiz0468 <bot.trial001@gmail.com>
2024-10-03 10:05:23 +0500

    update new.js file

--- new.js
+++ new.js
@@ -1,10 +1,8 @@
-/
+
 
 // Function to clear all tasks
 function clear_all_tasks() {
-    tasks = []; // Clear the tasks array
-    localStorage.removeItem("tasks"); // Remove tasks from localStorage
- // Update the task list to show no tasks
+
 }
 
 // Function to save tasks to localStorage
