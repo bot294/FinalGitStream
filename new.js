bcdf588ecd51655accb2a043bb37dd501352122a
= <bot.trial001@gmail.com>
2024-10-03 15:54:43 +0500

    parameter value changed

--- new.js
+++ new.js
@@ -1,20 +1,2 @@
-
-
-// Function to clear all tasks
-function clear_all_tasks(num) {
-    console.log("clear all tasks function");
-    if(num>0)
-    {
-        console.log("positive");
-    }
-    else{
-        console.log("negative");
-    }
-
-}
-
-// Function to save tasks to localStorage
-
-clear_all_tasks(0);
-// Display tasks on page load
-
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[1]); // banana
